4dd2395114b581c4d9d1c1f0ab83b8078fb2c7d9
へーさん <hira9603859504@gmail.com>
2020-04-25 17:31:52 +0900

    first commit

---
+++ src/components/Fish.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ info }}</p>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get('https://raw.githubusercontent.com/Hee-San/ACNH_data/master/fishes.json')
+      .then((response) => { this.info = response; });
+  },
+};
+</script>
+<style>
+</style>
