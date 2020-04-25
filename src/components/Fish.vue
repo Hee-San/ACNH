06005292098117fc41987e5e2e6c54605a111a2e
へーさん <hira9603859504@gmail.com>
2020-04-25 18:11:10 +0900

    make fish table

--- src/components/Fish.vue
+++ src/components/Fish.vue
@@ -1,5 +1,31 @@
 <template>
-  <p>{{ info }}</p>
+<div id="fish">
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Picture</th>
+        <th>Size</th>
+        <th>Cost</th>
+        <th>Place</th>
+        <th>Season</th>
+        <th>Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(fish, key) in fishes" :key="key">
+        <td>{{fish.Name}}</td>
+        <td>{{fish.Picture}}</td>
+        <td>{{fish.Size}}</td>
+        <td>{{fish.Cost}}</td>
+        <td>{{fish.Place}}</td>
+        <td>{{fish.Season}}</td>
+        <td>{{fish.Time}}</td>
+      </tr>
+    </tbody>
+  </table>
+  <!-- <p>{{ fishes }}</p> -->
+</div>
 </template>
 <script>
 import axios from 'axios';
@@ -7,13 +33,13 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      info: null,
+      fishes: null,
     };
   },
   mounted() {
     axios
       .get('https://raw.githubusercontent.com/Hee-San/ACNH_data/master/fishes.json')
-      .then((response) => { this.info = response; });
+      .then((response) => { this.fishes = response.data; });
   },
 };
 </script>
